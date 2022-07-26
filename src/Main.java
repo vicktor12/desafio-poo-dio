5c72a696912bc0cffa7cd5cb51d962fce3431257
vicktor12 <victorsouza0@icloud.com>
2022-07-26 16:08:37 -0300

    criação de novas classes Curso e Mentoria

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import br.com.brainstech.desafio.Dominio.Course;
+import br.com.brainstech.desafio.Dominio.Mentoring;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Course course = new Course();
+        Mentoring mentoring = new Mentoring();
+
+        course.setTitle("Objects Orientation");
+        course.setDescription("Notion of objects orientations in Java");
+        course.setWorkload(60);
+
+        mentoring.setTitle("Professor Camila");
+        mentoring.setDescription("Anything");
+        mentoring.setDate(LocalDate.now());
+
+        System.out.println(course);
+        System.out.println(mentoring);
+    }
+}
